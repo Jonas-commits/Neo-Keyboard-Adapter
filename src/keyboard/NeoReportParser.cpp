90a711bf4a7e5253fb26e18d8c1366b095d2a700
jonas <57602018+Jonas-commits@users.noreply.github.com>
2020-12-05 17:31:10 +0100

    1:1 mapping with neo modifier architecture working

--- src/keyboard/NeoReportParser.cpp
+++ src/keyboard/NeoReportParser.cpp
@@ -35,29 +35,85 @@ void NeoReportParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
 	*((uint8_t*)&afterMod) = after;
 
 	if (beforeMod.bmLeftCtrl != afterMod.bmLeftCtrl) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmLeftCtrl;
+		if (neoModifiers.bmLeftCtrl) {
+			OnKeyUp(after, KEY_LEFT_CTRL);
+			neoModifiers.bmLeftCtrl = false;
+		} else {
+			OnKeyDown(after, KEY_LEFT_CTRL);
+			neoModifiers.bmLeftCtrl = true;
+		}
 	}
+	
 	if (beforeMod.bmLeftShift != afterMod.bmLeftShift) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmLeftShift;
+		if (neoModifiers.bmLeftShift) {
+			OnKeyUp(after, KEY_LEFT_SHIFT);
+			neoModifiers.bmLeftShift = false;
+		} else {
+			OnKeyDown(after, KEY_LEFT_SHIFT);
+			neoModifiers.bmLeftShift = true;
+		}
 	}
+	
 	if (beforeMod.bmLeftAlt != afterMod.bmLeftAlt) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmLeftAlt;
+		if (neoModifiers.bmLeftAlt) {
+			OnKeyUp(after, KEY_LEFT_ALT);
+			neoModifiers.bmLeftAlt = false;
+		} else {
+			OnKeyDown(after, KEY_LEFT_ALT);
+			neoModifiers.bmLeftAlt = true;
+		}
+		
 	}
+	
 	if (beforeMod.bmLeftGUI != afterMod.bmLeftGUI) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmLeftGUI;
+		if (neoModifiers.bmLeftGUI) {
+			OnKeyUp(after, KEY_LEFT_GUI);
+			neoModifiers.bmLeftGUI = false;
+		} else {
+			OnKeyDown(after, KEY_LEFT_GUI);
+			neoModifiers.bmLeftGUI = true;			
+		}
 	}
 
 	if (beforeMod.bmRightCtrl != afterMod.bmRightCtrl) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmRightCtrl;
+		if (neoModifiers.bmRightCtrl) {
+			OnKeyUp(after, KEY_RIGHT_CTRL);
+			neoModifiers.bmRightCtrl = false;
+		} else {
+			OnKeyDown(after, KEY_RIGHT_CTRL);
+			neoModifiers.bmRightCtrl = true;
+		}
 	}
+	
 	if (beforeMod.bmRightShift != afterMod.bmRightShift) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmRightShift;
+		if (neoModifiers.bmRightShift) {
+			OnKeyUp(after, KEY_RIGHT_SHIFT);
+			neoModifiers.bmRightShift = false;
+		} else {
+			OnKeyDown(after, KEY_RIGHT_SHIFT);
+			neoModifiers.bmRightShift = true;
+		}
 	}
+	
 	if (beforeMod.bmRightAlt != afterMod.bmRightAlt) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmRightAlt;
+		if (neoModifiers.bmRightAlt) {
+			OnKeyUp(after, KEY_RIGHT_ALT);
+			neoModifiers.bmRightAlt = false;
+		} else {
+			OnKeyDown(after, KEY_RIGHT_ALT);
+			neoModifiers.bmRightAlt = true;
+		}
 	}
+	
 	if (beforeMod.bmRightGUI != afterMod.bmRightGUI) {
-		neoModifiers.bmLeftCtrl = !neoModifiers.bmRightGUI;
+		if (neoModifiers.bmRightGUI) {
+			OnKeyUp(after, KEY_RIGHT_GUI);
+			neoModifiers.bmRightGUI = false;
+		} else {
+			OnKeyDown(after, KEY_RIGHT_GUI);
+			neoModifiers.bmRightGUI = true;
+		}
+		
 	}
 }
 
