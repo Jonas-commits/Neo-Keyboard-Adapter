56e9b7ffde8f6eed96ce88cbd15dddc382389cfa
jonas <57602018+Jonas-commits@users.noreply.github.com>
2021-01-02 14:34:17 +0100

    first draft for compose, with 1 combination working

--- src/keyboard/Compose.cpp
+++ src/keyboard/Compose.cpp
@@ -1,5 +1,34 @@
 #include "Compose.h"
 
+/* 
+ * Little Endian! as only initialization of first union member is supported for
+ * C++ 11, and a union is unfortunately the only solution to initialize this
+ * in a ROM-saving way
+ */
+const Node Compose::root[] = { {{1, 0}, root}, {{L3, KEY_TAB}, l3Tab} };
+const Node Compose::l3Tab[] = { {{1, 0}, l3Tab}, {{L3, KEY_TAB}, uniNote} };
+const Node Compose::uniNote[] = { {{0x6B, 0x26}, nullptr} };
+
 uint16_t Compose::transition(uint8_t layer, uint8_t key){
+	Node* cur = currentNode;
+	uint16_t len = currentNode->x.data;
+	boolean found = false;
+	
+	//stupid linear search, could be optimized
+	for (uint16_t i = 0; i < len; i++){
+		cur++;
+		if(cur->x.layeredKey.layer == layer && cur->x.layeredKey.keycode == key) { //found
+			if (cur->nodes->nodes == nullptr) { //is leaf
+				currentNode = const_cast<Node*>(root);
+				return cur->nodes->x.data;
+			} else {
+				currentNode = const_cast<Node*>(cur->nodes);
+				return 1; 
+			}
+		}
+	}
+	
+	//not found
+	currentNode = const_cast<Node*>(root);
 	return 0;
 }
--- src/keyboard/Compose.h
+++ src/keyboard/Compose.h
@@ -3,19 +3,44 @@
 
 #include "NeoIncludes.h"
 
+/*
+* Node used as element in static defined lookup tree data structure.
+* The tree is static, therefore no methods for removing/adding elements,
+* everything has to be done in this class definition. 
+* 
+*  When first element in list: data taken from union,
+  data = list length when nodes != nullptr,
+  data = Unicode character when nodes == nullptr
+  
+  When not first element in list: layeredKey taken from union, 
+  nodes = first list element of list to continue search for next keystroke
+*/
+
+struct Node {
+	union {
+		struct {
+			uint8_t layer;
+			uint8_t keycode;
+		} layeredKey;
+		uint16_t data;
+	} x;
+	Node* nodes;
+};
+
 class Compose
 {
-//variables
-public:
-protected:
-private:
+	private:
+	Node* currentNode;
 
-//functions
-public:
-	Compose(){};
+	//the lookup data
+	const static Node root[];
+	const static Node l3Tab[];
+	const static Node uniNote[];
+
+	public:
+	Compose() : currentNode(const_cast<Node*>(root)){}
+		
 	uint16_t transition(uint8_t layer, uint8_t key);
-protected:
-private:
 
 }; //Compose
 
