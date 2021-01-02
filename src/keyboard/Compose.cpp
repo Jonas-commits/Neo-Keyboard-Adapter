ffbba2723b31f9c524d2c9911d8a3a0ce2be8338
jonas <57602018+Jonas-commits@users.noreply.github.com>
2021-01-02 17:07:05 +0100

    most of the time, the list length will be one, let's cover that case to save the length element in that case

--- src/keyboard/Compose.cpp
+++ src/keyboard/Compose.cpp
@@ -3,35 +3,33 @@
 /* 
  * Little Endian, hack to give the information of length directly instead of null-termination
  */
-const Node Compose::root[] = { {{1, 0}, root}, 
+const Node Compose::root[] = { {{2, 0}, nullptr}, 
 	{{L3, KEY_TAB}, l3Tab}
 };
 	
-const Node Compose::l3Tab[] = { {{3, 0}, l3Tab}, 
+const Node Compose::l3Tab[] = { {{4, 0}, nullptr}, 
 	{{L1, KEY_G}, l3TabG},
 	{{L3, KEY_Q}, l3TabL3Q},
 	{{L3, KEY_TAB}, composeSymbols}
 };
 
-const Node Compose::l3TabG[] = { {{1, 0}, l3TabG},
-	{{L1, KEY_SPACE}, composeSymbols + 1}
-};
+const Node Compose::l3TabG[] = { {{L1, KEY_SPACE}, composeSymbols + 1} };
 
-const Node Compose::l3TabL3Q[] = { {{1, 0}, l3TabL3Q},
-	{{L1, KEY_G}, composeSymbols + 1}
-};
+const Node Compose::l3TabL3Q[] = { {{L1, KEY_G}, composeSymbols + 1} };
 
 const uint16_t Compose::composeSymbols[] = {0x266B, 0x25CC};
 
-uint16_t Compose::transition(uint8_t layer, uint8_t key){
+uint16_t Compose::transition(uint8_t layer, uint8_t key) {
 	Node* cur = currentNode;
-	uint16_t len = currentNode->x.len;
+	uint16_t len = (currentNode->ptr.value == nullptr) ? cur->x.len : 1; 
+
 	boolean found = false;
 	
 	//stupid linear search, could be optimized
-	for (uint16_t i = 0; i < len; i++){
-		cur++;
-		if(cur->x.layeredKey.layer == layer && cur->x.layeredKey.keycode == key) { //found
+	for (uint16_t i = 0; i < len; i++) {
+		if(cur->ptr.value != nullptr && //discard length element
+			cur->x.layeredKey.layer == layer && 
+			cur->x.layeredKey.keycode == key) { //found
 			
 			//pointer points to element of composeSymbols List
 			if (cur->ptr.value >= (void*) composeSymbols && 
@@ -45,6 +43,7 @@ uint16_t Compose::transition(uint8_t layer, uint8_t key){
 				return 1; 
 			}
 		}
+		cur++;
 	}
 	
 	//not found
